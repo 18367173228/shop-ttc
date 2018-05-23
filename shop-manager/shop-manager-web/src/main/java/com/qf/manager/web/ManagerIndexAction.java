d7e4f97febcb1c76597aa5b1865e7de9eea94456
tongtingchang <791917705@qq.com>
2018-05-23 16:59:58 +0800

    1111

---
+++ shop-manager/shop-manager-web/src/main/java/com/qf/manager/web/ManagerIndexAction.java
@@ -0,0 +1,17 @@
+package com.qf.manager.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Administrator on 2018/5/22.
+ */
+@Controller
+public class ManagerIndexAction {
+
+    @RequestMapping(value="/ttc",method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+}
